53786e8910f44284fdc305857fa9ade669256834
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-02-25 17:15:26 +0000

    Basic geolocator, may not work...

---
+++ src/Impek/Gen/GeoLocation.java
@@ -0,0 +1,60 @@
+package Impek.Gen;
+
+import android.location.*;
+import android.os.Bundle;
+import android.content.*;
+import java.util.*;
+
+public class GeoLocation extends Observable {
+
+	LocationManager locationManager;
+	Location location;
+	LocListener listener;
+	
+	public GeoLocation()
+	{
+		LocationManager locationManager = (LocationManager) ImpekActivity.curr.getSystemService(Context.LOCATION_SERVICE);
+		listener = new LocListener();
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 5, listener);
+	}
+	
+	double getLattitude()
+	{
+		return location.getLatitude();
+	}
+	
+	double getLongitude()
+	{
+		return location.getLongitude();
+	}
+	
+	double getAccuracy()
+	{
+		return location.getAccuracy();
+	}
+	
+	private class LocListener implements LocationListener
+	{
+
+		public void onLocationChanged(Location loc) {
+			location = loc;
+		}
+
+		public void onProviderDisabled(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onProviderEnabled(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+}
