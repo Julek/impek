0fa31364f3c9321d8ff4df78734c48594e2d8eb7
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-02-25 17:15:04 +0000

    Basic geolocator, may not work...

--- src/Impek/Gen/ImpekActivity.java
+++ src/Impek/Gen/ImpekActivity.java
@@ -1,14 +1,21 @@
 package Impek.Gen;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 public class ImpekActivity extends Activity {
     /** Called when the activity is first created. */
+
+	static Context curr;
 	
-	@Override
+	static GeoLocation locator;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        curr = this;
+        locator = new GeoLocation();
+        System.out.println("Latitude: " + locator.getLattitude() + "\nLongitude: " + locator.getLattitude());
     }
 }
