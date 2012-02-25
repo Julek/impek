b04629bfcfba2295321a2722999c6c3a5278713b
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-02-25 17:16:46 +0000

    Removed merge tags

--- src/Impek/Gen/ImpekActivity.java
+++ src/Impek/Gen/ImpekActivity.java
@@ -5,14 +5,11 @@ import android.content.Context;
 import android.os.Bundle;
 
 public class ImpekActivity extends Activity {
-<<<<<<< HEAD
     /** Called when the activity is first created. */
 
 	static Context curr;
 	
 	static GeoLocation locator;
-=======
->>>>>>> 55e41697c156e25b184594f1eee0c0582319d7d8
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
