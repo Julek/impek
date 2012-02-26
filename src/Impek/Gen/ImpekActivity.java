d446760091d33083fc4dd2778bc5b77880306448
Houssem El Fekih <he610@doc.ic.ac.uk>
2012-02-26 12:04:28 +0000

    plugged reverse for testing

--- src/Impek/Gen/ImpekActivity.java
+++ src/Impek/Gen/ImpekActivity.java
@@ -32,11 +32,31 @@ public class ImpekActivity extends Activity {
 		
 	//Commenting this out for my testing (Julek)
 	
-	/*public void update() {
-		EditText s = ((EditText) findViewById(R.id.editText1));
+	public void update() {
+		
+		
+		TextView r = ((TextView) findViewById(R.id.textView1));
+		TextView v = ((TextView) findViewById(R.id.textView2));
+
+		try {
+			double latitude = GeoLocation.getLattitude();
+			double longitude = GeoLocation.getLongitude();
+			v.setText("Lat: " + latitude + "\nLon: " + longitude);
+			r.setText("Lat: " + latitude);
+		} catch (NoLocationError e) {
+			r.setText("N/A");
+		}
+		try {
+			double longitude = GeoLocation.getLongitude();
+			v.setText("Long.: " + longitude);
+		} catch (NoLocationError e) {
+			v.setText("N/A");
+		}
+		
+		TextView s = ((TextView) findViewById(R.id.textView3));
 		//EditText v = ((EditText) findViewById(R.id.editText2));
 		//@testing and experimenting ui:
-		reverseTest("Reverse Lookup");
+		//reverseTest("Reverse Lookup");
 	
 		
 		Button j = ((Button)findViewById(R.id.button1));
@@ -50,6 +70,21 @@ public class ImpekActivity extends Activity {
 			}
 			
 		});
+		
+		
+		try {
+			try{
+				s.setText(GeoConversion.reverseGeocode(GeoLocation.getLocation()));
+			}
+			catch(NoGeoConversion e)
+			{
+				s.setText("N/A");
+			}
+			
+		} catch (NoLocationError e) {
+			s.setText("N/A");
+		} 
+	
 	}
 	
 	
@@ -78,9 +113,7 @@ public class ImpekActivity extends Activity {
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.mainmenu, menu);
 	    return true;
-	}*/
-	
-	
+	}
 	
 
 	
@@ -95,37 +128,26 @@ public class ImpekActivity extends Activity {
 		rs.setText(r);
 	}
 		
-	public void update() {
-		TextView s = ((TextView) findViewById(R.id.textView1));
-		//TextView v = ((TextView) findViewById(R.id.textView2));
+//	public void update() {
+//		TextView s = ((TextView) findViewById(R.id.textView1));
+//		//TextView v = ((TextView) findViewById(R.id.textView2));
+//
+//		/*try {
+//			double latitude = GeoLocation.getLattitude();
+//			double longitude = GeoLocation.getLongitude();
+//			v.setText("Lat: " + latitude + "\nLon: " + longitude);
+//			s.setText("Lat: " + latitude);
+//		} catch (NoLocationError e) {
+//			s.setText("N/A");
+//		}
+//		try {
+//			double longitude = GeoLocation.getLongitude();
+//			v.setText("Long.: " + longitude);
+//		} catch (NoLocationError e) {
+//			v.setText("N/A");
+//		}*/
+//
 
-		/*try {
-			double latitude = GeoLocation.getLattitude();
-			double longitude = GeoLocation.getLongitude();
-			v.setText("Lat: " + latitude + "\nLon: " + longitude);
-			s.setText("Lat: " + latitude);
-		} catch (NoLocationError e) {
-			s.setText("N/A");
-		}
-		try {
-			double longitude = GeoLocation.getLongitude();
-			v.setText("Long.: " + longitude);
-		} catch (NoLocationError e) {
-			v.setText("N/A");
-		}*/
-
-		try {
-			try{
-				s.setText(GeoConversion.reverseGeocode(GeoLocation.getLocation()));
-			}
-			catch(NoGeoConversion e)
-			{
-				s.setText("N/A");
-			}
-			
-		} catch (NoLocationError e) {
-			s.setText("N/A");
-		} 
-		
-	}
+//		
+//	}
 }
