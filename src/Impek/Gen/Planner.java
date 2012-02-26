5ea096aaa54510f70e0f5c54121e85fd80bbae45
Alex <alexandru.paunoiu10@imperial.ac.uk>
2012-02-26 14:50:51 +0000

    NOT NOT NOT TESTEDgit pushgit pushgit pushgit pushgit pushgit pushgit pushgit pushgit pushgit pushgit pushgit push COULD SCREW UP REALLY BADLYgit push! BEWARE

--- src/Impek/Gen/Planner.java
+++ src/Impek/Gen/Planner.java
@@ -20,9 +20,13 @@ public class Planner extends Observable implements Observer {
 	private Timer appTimer;
 	private Settings set;
 	
+	private double current_lat;
+	private double current_long;
+	private String current_post;
+	
 	public Planner(Context context) {
 		set = new Settings(context);
-		wsc = new WebServicesCom("http://4m9a.localtunnel.com/tfl");
+		wsc = new WebServicesCom("http://tfl-data.herokuapp.com/tfl");
 		wsc.addObserver(this);
 		appTimer = new Timer();
 		appTimer.schedule(new PlannerTimerTask(), 100,1000);
@@ -50,22 +54,29 @@ public class Planner extends Observable implements Observer {
 		}
 	}
 	
-	public void addLocation(String src_post, 
-							String dst_post) {
-		wsc.addLocation("locator", src_post, "locator", dst_post);
-	}
 	
 	public void notificationOfPosition( double latitude, double longitude, 
 										String postcode ) {
-		
+		current_lat = latitude;
+		current_long = longitude;
+		current_post = postcode;
+		Iterator<SettingsNode> it = set.iterator();
+		while(it.hasNext()) {
+			SettingsNode sn = it.next();
+			wsc.addLocation("locator", current_post, 
+							"locator", sn.getPostcode());
+		}
 	}
 	
 	public void addNewEntry(String locName, double latitude, double longitude,
-							Date arrival) {
+							Date arrival, String postcode) {
 		if(!set.pushSettingsNode(locName, latitude, longitude, arrival, 
-							false, new Date())) 
+							false, new Date(), postcode)) 
 		{
 			//notify ui of problem
+		}
+		else {
+			notificationOfPosition(current_lat, current_long, current_post);
 		}
 	}
 	
@@ -87,6 +98,7 @@ public class Planner extends Observable implements Observer {
 					for(int i = 0;i<js.length();i++) {		
 						rs[i] = new Route();
 						JSONArray jsarrpath = js.getJSONObject(i).getJSONArray("option");
+						Time duration = new Time();
 						for(int j = 0;j<jsarrpath.length();j++) {
 							JourneyNode n = new JourneyNode();
 							JSONObject jsobj = jsarrpath.getJSONObject(j);
@@ -97,6 +109,7 @@ public class Planner extends Observable implements Observer {
 							Time t2 = new Time(v[1]);
 							n.setArrivalTime(t2);
 							n.setPartialTime(t1.substract(t2));
+							duration = duration.addition(n.getArrivalTime());
 							String type = jsobj.getString("type");
 							if(type == "walk") {
 								n.setMode(JourneyNode.Mode.Walk);
@@ -109,8 +122,38 @@ public class Planner extends Observable implements Observer {
 							}
 							rs[i].addNodeToRoute(n);
 						}
+						rs[i].setDuration(duration);
 					}
-					//do route thing...
+					Iterator<SettingsNode> it = set.iterator();
+					while(it.hasNext()) {
+						SettingsNode sn = it.next();
+						for(int j = 0;j<js.length();j++)
+							if(sn.getPostcode().toLowerCase().equals(rs[j].getNode(rs[j].noNodes()-1).getOrigin().toLowerCase())) {
+								JourneyNode jn = rs[j].getNode(rs[j].noNodes()-1);
+								Date d = new Date();
+								d.setHours(jn.getArrivalTime().getHours());
+								d.setMinutes(jn.getArrivalTime().getMinutes());
+								
+								Time t = new Time(sn.getDate().getHours(), sn.getDate().getMinutes());
+								t = t.substract(jn.getArrivalTime());
+								
+								
+								if(sn.getDate().getDay() == d.getDay() && sn.getDate().getMonth() == d.getMonth() 
+																	   && sn.getDate().getYear() == d.getYear())
+									if(sn.getDate().compareTo(d) == -1 && t.getHours() == 0 
+										&& t.getMinutes() <= 10) {
+										sn.setScheduled(true);
+										Time dur = rs[j].getDuration();
+										d.setHours(d.getHours() - dur.getHours());
+										d.setMinutes(d.getMinutes() - dur.getMinutes());
+										sn.setDepDate(d);
+										setChanged();
+										notifyObservers((Object)rs[j]);
+									}
+								
+								
+							}
+					}
 				}
 			} catch (JSONException e) {
 				//should never get here :)
