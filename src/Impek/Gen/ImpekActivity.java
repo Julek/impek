f0589babebe3f58c310babd505c477a3864efbdc
Houssem El Fekih <he610@doc.ic.ac.uk>
2012-02-26 02:33:11 +0000

    ui stuff ..

--- src/Impek/Gen/ImpekActivity.java
+++ src/Impek/Gen/ImpekActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.LabeledIntent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,35 +15,30 @@ import android.util.Log;
 public class ImpekActivity extends Activity {
 	/** Called when the activity is first created. */
 
-	String [] serv = {
-			"eat pizza @dio"
-			,"make love @home"
-			,"listen to the music @yh ok"
-			,"get markee @airport"
-	};
 	static Context curr;
 
-	public void testHoussem1(){
-		
-		for(int i = 0 ; i < serv.length;i++){
-		
-		TextView r = new TextView(this);
-		
-		r.setText(serv[i]);
-			
-		}
-		
-	}
-	
-	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		curr = this;
 		GeoLocation.setup_GeoLocation();
 		update();
-		reverseTest("Lookup temp.");
+		reverseTest("Reverse Lookup");
 	}
+	
+//public void testHoussem1(){
+//		
+//		for(int i = 0 ; i < serv.length;i++){
+//		
+//		TextView r = new TextView(this);
+//		
+//		r.setText(serv[i]);
+//			
+//		}
+//		
+//	}
+//	
+//	
 	
 	public void reverseTest(String r){
 		TextView rs = ((TextView) findViewById(R.id.textView3));
