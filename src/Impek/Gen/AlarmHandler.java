d7a6b156fb6e40b4718c654916c090f5751b1a14
Houssem El Fekih <he610@doc.ic.ac.uk>
2012-02-26 14:33:02 +0000

    can't be bothered to comment , some stuff

--- src/Impek/Gen/AlarmHandler.java
+++ src/Impek/Gen/AlarmHandler.java
@@ -1,13 +1,22 @@
 package Impek.Gen;
 
+import java.util.Iterator;
+
 import android.app.Activity;
+import android.app.LauncherActivity.ListItem;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class AlarmHandler extends Activity{
 	static Context curr;
-	Settings s = new Settings(this);
+	Planner s;
 	
+	public AlarmHandler(Planner p) {
+		// TODO Auto-generated constructor stub
+		s = p;
+	}
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ShowPath();
@@ -17,9 +26,20 @@ public class AlarmHandler extends Activity{
 	}
 
 	private void ShowPath() {
+		Settings r = s.getSettingsInstance();
+		Iterator<SettingsNode> i = r.iterator();
+		ListView s = new ListView(curr);
 		
-		Iterator i = Settings.
-		
+		while(i.hasNext()){
+			SettingsNode currNode = i.next();
+			
+			if(currNode.isScheduled()){
+				TextView d = new TextView(curr);
+				d.setText(currNode.getLocationName()+ " in "+currNode.getDepDate());
+				s.addView(d);
+			}
+			//currNode.
+		}
 	}
 	
 	
